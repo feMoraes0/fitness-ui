ee59f0f86f1674d860e47b53d0399e51a6f29d16
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-09-23 22:40:20 -0300

    Adding Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Geometria'),
-      home: Home(),
+      home: Tabs(),
     );
   }
 }
+
+class Tabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: TabBarView(
+          children: <Widget>[
+            Home(),
+            Container(
+              color: Colors.redAccent,
+            ),
+            Container(
+              color: Colors.blueAccent,
+            ),
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.explicit, size: 26.0),
+            ),
+            Tab(
+              icon: Icon(Icons.restaurant_menu, size: 26.0),
+            ),
+            Tab(
+              icon: Icon(Icons.insert_chart, size: 26.0),
+            ),
+          ],
+          labelPadding: EdgeInsets.all(5.0),
+          labelColor: Colors.blue,
+          unselectedLabelColor: Colors.black12,
+          indicatorWeight: 0.01,
+        ),
+      ),
+    );
+  }
+}
+
 
 class Home extends StatelessWidget {
   @override
