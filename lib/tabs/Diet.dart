074db114f53986f4e7772554079a5d71c7e770f0
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-09-25 23:15:40 -0300

    Added internal tabs to Diet

--- lib/tabs/Diet.dart
+++ lib/tabs/Diet.dart
@@ -4,37 +4,70 @@ import 'package:flutter/material.dart';
 class Diet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            padding: EdgeInsets.only(top: 20.0),
-            child: Column(
-              children: <Widget>[
-                Header(
-                  "Diet",
-                  rightSide: Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                      vertical: 8.0,
-                    ),
-                    margin: EdgeInsets.only(right: 20.0),
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(100, 140, 255, 1.0),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Tracker",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w900,
-                        ),
+    return Container(
+      padding: EdgeInsets.only(top: 20.0),
+      child: SafeArea(
+        child: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              flexibleSpace: Header(
+                "Diet",
+                rightSide: Container(
+                  height: 35.0,
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  margin: EdgeInsets.only(right: 20.0),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(100, 140, 255, 1.0),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Tracker",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
                       ),
                     ),
                   ),
                 ),
-              ],
+              ),
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Container(
+                    height: 30.0,
+                    child: Tab(
+                      text: "Breakfast",
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    child: Tab(
+                      text: "Lunch",
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    child: Tab(
+                      text: "Dinner",
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    child: Tab(
+                      text: "Snacks",
+                    ),
+                  ),
+
+                ],
+                labelColor: Colors.black87,
+                unselectedLabelColor: Colors.grey[400],
+                indicatorWeight: 4.0,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: Color.fromRGBO(215, 225, 255, 1.0),
+              ),
             ),
           ),
         ),
