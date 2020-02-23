2ed2fe1d1cd5d09764453dca56a2f56d0cbc4a28
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-02-23 12:37:21 +0100

    refactor: created model to exercises

---
+++ lib/models/exercise.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class Exercise {
+  final String title, time, difficult, image;
+
+  Exercise({
+    @required this.title,
+    @required this.time,
+    @required this.difficult,
+    @required this.image,
+  });
+}
