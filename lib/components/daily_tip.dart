8178681353d210e67317e5bd2eee3bff0a428454
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-02-24 14:54:30 +0100

    refactor: added image asset and daily tip name pattern with map data

--- lib/components/daily_tip.dart
+++ lib/components/daily_tip.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DailyTip extends StatelessWidget {
+
+  final Map<String, String> element = {
+    'title': '3 Main workout tips',
+    'subtitle': 'The American Council on Exercises (ACE) recently surveyed 3,000 ACE-certificated personal trainers about the best!',
+    'image': 'assets/images/image011.jpg',
+  };
+
   @override
   Widget build(BuildContext context) {
-    var width = 330.0;
+    Size size = MediaQuery.of(context).size;
+    double width = size.width * 0.80;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -16,8 +24,7 @@ class DailyTip extends StatelessWidget {
           ),
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: NetworkImage(
-                  "https://www.wallpaperup.com/uploads/wallpapers/2015/01/13/584666/bc6cbb316c0a71e0db4c7ee8aeb8ff7c.jpg"),
+              image: AssetImage(this.element['image']),
               fit: BoxFit.fill,
             ),
             borderRadius: BorderRadius.all(
@@ -26,15 +33,14 @@ class DailyTip extends StatelessWidget {
           ),
         ),
         Text(
-          "3 Main workout tips",
+          this.element['title'],
           style: TextStyle(fontSize: 14.0),
         ),
         Container(
           width: width,
           margin: EdgeInsets.only(top: 10.0),
           child: Text(
-            "The American Council on Exercises (ACE) recently surveyed 3,000"
-            " ACE-certificated personal trainers about the best!",
+            this.element['subtitle'],
             style: TextStyle(
               color: Colors.black45,
               fontSize: 14.0,
@@ -48,7 +54,7 @@ class DailyTip extends StatelessWidget {
             horizontal: 15.0,
           ),
           child: Text(
-            "More",
+            'More',
             style: TextStyle(
               color: Colors.white,
             ),
