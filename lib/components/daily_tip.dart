2e465504ac338c2942f72c35707a4a65a7499358
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-02-24 14:46:42 +0100

    style: increased code readability

--- lib/components/daily_tip.dart
+++ lib/components/daily_tip.dart
@@ -10,34 +10,56 @@ class DailyTip extends StatelessWidget {
         Container(
           width: width,
           height: 200.0,
-          margin: EdgeInsets.only(right: 15.0, bottom: 10.0,),
+          margin: EdgeInsets.only(
+            right: 15.0,
+            bottom: 10.0,
+          ),
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: NetworkImage("https://www.wallpaperup.com/uploads/wallpapers/2015/01/13/584666/bc6cbb316c0a71e0db4c7ee8aeb8ff7c.jpg"),
+              image: NetworkImage(
+                  "https://www.wallpaperup.com/uploads/wallpapers/2015/01/13/584666/bc6cbb316c0a71e0db4c7ee8aeb8ff7c.jpg"),
               fit: BoxFit.fill,
             ),
-            borderRadius: BorderRadius.all(Radius.circular(15.0))
+            borderRadius: BorderRadius.all(
+              Radius.circular(15.0),
+            ),
           ),
         ),
-        Text("3 Main workout tips", style: TextStyle(fontSize: 14.0),),
+        Text(
+          "3 Main workout tips",
+          style: TextStyle(fontSize: 14.0),
+        ),
         Container(
           width: width,
           margin: EdgeInsets.only(top: 10.0),
           child: Text(
             "The American Council on Exercises (ACE) recently surveyed 3,000"
-              " ACE-certificated personal trainers about the best!",
-            style: TextStyle(color: Colors.black45, fontSize: 14.0),
+            " ACE-certificated personal trainers about the best!",
+            style: TextStyle(
+              color: Colors.black45,
+              fontSize: 14.0,
+            ),
           ),
         ),
         Container(
           margin: EdgeInsets.only(top: 10.0),
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-          child: Text("More", style: TextStyle(color: Colors.white)),
+          padding: EdgeInsets.symmetric(
+            vertical: 5.0,
+            horizontal: 15.0,
+          ),
+          child: Text(
+            "More",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            borderRadius: BorderRadius.all(
+              Radius.circular(20.0),
+            ),
             color: Colors.lightBlue,
           ),
-        )
+        ),
       ],
     );
   }
