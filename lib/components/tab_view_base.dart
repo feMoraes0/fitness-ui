fdbebfa1367cc7eab021c286401b3e08c087709a
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-02-24 15:35:03 +0100

    refactor: asset images and pattern names

---
+++ lib/components/tab_view_base.dart
@@ -0,0 +1,71 @@
+import 'package:fitness_flutter/components/image_card_with_basic_footer.dart';
+import 'package:fitness_flutter/data/Dishes.dart';
+import 'package:fitness_flutter/models/exercise.dart';
+import 'package:flutter/material.dart';
+
+class TabViewBase extends StatelessWidget {
+  final String tabName;
+
+  TabViewBase({@required this.tabName});
+
+  List<Widget> _renderItem(Size size) {
+    return List<Widget>.generate(dishes.length, (index) {
+      var tag = dishes[index]["title"] + index.toString();
+      Exercise exercise = Exercise(
+        image: dishes[index]["image"],
+        title: dishes[index]["title"],
+        time: dishes[index]["time"],
+        difficult: dishes[index]["calories"],
+      );
+      return Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        child: ImageCardWithBasicFooter(
+          exercise: exercise,
+          tag: tag,
+          imageWidth: size.width,
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        width: size.width,
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(219, 228, 255, 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Track my " + this.tabName,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Color.fromRGBO(122, 158, 255, 1.0),
+                    ),
+                  ),
+                  Icon(
+                    Icons.add,
+                    size: 25.0,
+                    color: Color.fromRGBO(122, 158, 255, 1.0),
+                  )
+                ],
+              ),
+            ),
+            Column(children: this._renderItem(size)),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/data/Dishes.dart
+++ lib/data/Dishes.dart
@@ -1,24 +1,24 @@
 List<Map> dishes = [
   {
-    "image": "https://images.scrippsnetworks.com/up/tp/Scripps_-_Food_Category_Prod/572/171/0139045_16x9.jpg",
+    "image": "assets/images/image012.jpg",
     "title": "Dish 01",
     "time": "25 min",
     "calories": "426 kCal"
   },
   {
-    "image": "https://images.ctfassets.net/wy4h2xf1swlt/asset_70000/2bc4822e34f73b7117fe0ebf94ef951e/391077ae-istock-839805588.jpg",
+    "image": "assets/images/image013.jpg",
     "title": "Dish 02",
     "time": "10 min",
     "calories": "200 kCal"
   },
   {
-    "image": "https://www.sheknows.com/wp-content/uploads/2018/12/fd7m0hsm2k1yvljsuikr.jpeg",
+    "image": "assets/images/image014.jpeg",
     "title": "Dish 03",
     "time": "40 min",
     "calories": "634 kCal"
   },
   {
-    "image": "https://hips.hearstapps.com/del.h-cdn.co/assets/cm/15/10/54f65d873c8b2_-_tuscan-turkey-burger-rbk0413-recipe-bkf1b6-s2nlgs-s2.jpg",
+    "image": "assets/images/image015.jpg",
     "title": "Dish 04",
     "time": "60 min",
     "calories": "1.238 kCal"
